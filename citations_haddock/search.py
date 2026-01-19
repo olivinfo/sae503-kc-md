1238da77cbdd507e5d7dc45746153ea1376e86f5
Killian Chesnot <50640940+0xKILL1@users.noreply.github.com>
2026-01-19 12:03:42 +0100

    Update search.py

--- citations_haddock/search.py
+++ citations_haddock/search.py
@@ -1,5 +1,4 @@
 import os
-import csv
 from flask import Flask, request, jsonify
 from redis import Redis
 from flasgger import Swagger
@@ -7,14 +6,12 @@ from functools import wraps
 
 REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
 REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
-REDIS_DB = int(os.getenv("REDIS_DB", 0))
-APP_PORT = int(os.getenv("APP_PORT", 5001))
+APP_PORT = int(os.getenv("APP_PORT", 5002))
 ADMIN_KEY = os.getenv("ADMIN_KEY", "default_key")
-CSV_FILE_QUOTES = os.getenv("CSV_FILE_QUOTES", "initial_data_quotes.csv")
 
 app = Flask(__name__)
 swagger = Swagger(app)
-redis_client = Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB, decode_responses=True)
+redis_client = Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
 
 def require_auth(f):
     @wraps(f)
@@ -25,42 +22,20 @@ def require_auth(f):
         return f(*args, **kwargs)
     return decorated
 
-# Chargement initial des données citations
-if not redis_client.exists("quotes:1"):
-    if os.path.exists(CSV_FILE_QUOTES):
-        with open(CSV_FILE_QUOTES, mode='r', encoding='utf-8') as file:
-            reader = csv.DictReader(file)
-            for row in reader:
-               quote = row['quote']
-               quote_id = redis_client.incr("quote_id")
-               redis_client.hset(f"quotes:{quote_id}", mapping={"quote": quote})
-               redis_client.sadd("quotes", f"quotes:{quote_id}")
-
-@app.route('/quotes', methods=['GET'])
-def get_quotes():
-    quotes = redis_client.smembers("quotes")
-    quote_list = [redis_client.hgetall(q) for q in quotes]
-    return jsonify(quote_list), 200
-
-@app.route('/quotes', methods=['POST'])
+@app.route('/search', methods=['GET'])
 @require_auth
-def add_quote():
-    data = request.get_json()
-    user_id, quote = data.get("user_id"), data.get("quote")
-    if not user_id or not quote:
-        return jsonify({"error": "user_id et quote sont requis"}), 400
-    quote_id = redis_client.incr("quote_id")
-    redis_client.hset(f"quotes:{quote_id}", mapping={"user_id": user_id, "quote": quote})
-    redis_client.sadd("quotes", f"quotes:{quote_id}")
-    return jsonify({"message": "Citation ajoutée", "id": quote_id}), 201
-
-@app.route('/quotes/<int:quote_id>', methods=['DELETE'])
-@require_auth
-def delete_quote(quote_id):
-    if not redis_client.hexists(f"quotes:{quote_id}","quote"):
-        return jsonify({"error": "Citation non trouvée"}), 404
-    redis_client.hdel(f"quotes:{quote_id}","quote")
-    return jsonify({"message": "Citation supprimée"}), 200
+def search_quotes():
+    keyword = request.args.get("keyword")
+    if not keyword:
+        return jsonify({"error": "Mot-clé requis"}), 400
+    members = redis_client.smembers("quotes")
+    filtered_quotes = []
+    for member in members:
+        quote_object = redis_client.hgetall(member)
+        quote = quote_object.get("quote","")
+        if keyword.lower() in quote.lower():
+            filtered_quotes.append(quote)
+    return jsonify(filtered_quotes), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=APP_PORT)
