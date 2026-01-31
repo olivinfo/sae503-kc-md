8d9372220a617e9b01da83f6ee9ef5e1e2179904
0xKILL1 <root@localhost.local>
2026-01-31 13:31:09 +0100

    remove testHelm

--- haddock_kc_md/testHelm.sh
+++
@@ -1,3 +0,0 @@
-helm install haddock .
-kubectl create secret docker-registry regcred   --docker-server=ghcr.io   --docker-username=0xKILL1   --docker-password=   --namespace=haddock
-kubectl rollout restart deployment -n haddock
